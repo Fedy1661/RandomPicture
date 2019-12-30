1000cf9d6784fc39513a8f62bfafd29ca18ec9b1
Fedor <fedykonovalov@gmail.com>
2019-12-30 13:58:01 +0300

    Updates

---
+++ precache-manifest.d433753bdacab260735251f7b1741cb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0092561d22498393d884823b166f1d",
+    "url": "/RandomPicture/index.html"
+  },
+  {
+    "revision": "45cbbc08f30cb2a164b1",
+    "url": "/RandomPicture/static/css/main.2ff5c228.chunk.css"
+  },
+  {
+    "revision": "91abd98bb38cbbc516f9",
+    "url": "/RandomPicture/static/js/2.0e6f255c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RandomPicture/static/js/2.0e6f255c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45cbbc08f30cb2a164b1",
+    "url": "/RandomPicture/static/js/main.bc99b856.chunk.js"
+  },
+  {
+    "revision": "4de0b84bdb6c4716584c",
+    "url": "/RandomPicture/static/js/runtime-main.9fe9c0ce.js"
+  }
+]);
